5d948f8b164f06f69f59c638cf891c8f1cf58908
Michael Oha <michaeloha@yahoo.ca>
2020-06-03 02:56:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,81 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-// Player 1
-// var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
-//
-// var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png -dice6.png
-//
-// var randomImageSource = "images/" + randomDiceImage; //images/dice1.png- images/dice6.png
-//
-// var image1 = document.querySelectorAll("img")[0];
-//
-// image1.setAttribute("src", randomImageSource);
-//
-// //player 2
-//   // document.querySelectorAll("img")[1].setAttribute("src", "images/dice" +  (Math.floor(Math.random() * 6) + 1) + ".png");
-// var randomNumber2 = Math.floor(Math.random() * 6) + 1
-//
-// var randomImageSource2 ="images/dice" + randomNumber2 + ".png";
-//
-// document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-//
-// // Winner or draw (if/Else Statement)
-//
-// if (randomNumber1 > randomNumber2) {
-//   document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
-//
-// }
-// else if (randomNumber2 > randomNumber1) {
-//   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-// }
-//
-// else {
-//   document.querySelector("h1").innerHTML = "It's a Draw!";
-//
-// }
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-//      Thurs 14/5/20  L-165  practice on my own
-// Player 1
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
-var randomImageSource= "images/dice" + randomNumber1 + ".png";
-var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-// Player 2  // not a must to added var to document.querySelectorAll( see below)
-var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
-var randomImageSource2= "images/dice" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+    var buttonInnerHTML = this.innerHTML;
 
-if(randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = "Player 1 Wins 🚩";
-} else if(randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML ="🚩 Player 2 Wins";
+    makeSound(buttonInnerHTML);
 
-}else{
-  document.querySelector("h1").innerHTML = "Draw ";
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
 
 }
